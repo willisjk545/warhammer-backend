5c1e42f79c7061318c12132bd4864de3d98f4eaa
willisjk545 <51234717+willisjk545@users.noreply.github.com>
2021-04-06 15:12:53 -0400

    Added Units Context

---
+++ Models/Units.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _40K.Models
+{
+    public partial class Units
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ArmyID { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+    }
+}

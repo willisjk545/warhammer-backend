bbad464548b865996fc3e73e7a0b8bcd9094f304
willisjk545 <51234717+willisjk545@users.noreply.github.com>
2021-11-04 11:22:19 -0400

    Setting up AWS Lambda

---
+++ LambdaEntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureApp.WebApi
+{
+    public class LambdaEntryPoint: Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>();
+        }
+    }
+}
